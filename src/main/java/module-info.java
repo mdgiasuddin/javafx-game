3fae7b65eb8afa772d759b26ef69f244f22e7d32
Md. Giash Uddin <giash.inument@bkash.com>
2026-08-28 07:56:18 +0600

    Remove chess and carom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,7 @@ module com.example.javagame {
     opens com.example.javagame.caromnetwork to javafx.graphics;
 
 
-    exports com.example.javagame.chess;
     exports com.example.javagame.chessnetwork;
     exports com.example.javagame.tetris;
-    exports com.example.javagame.carom;
     exports com.example.javagame.caromnetwork;
-
 }
